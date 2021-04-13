1acfc6311e3fcf095c21528c043669f399d0cc09
Courtney Amos <c.amos@shaw.ca>
2021-04-12 18:19:50 -0700

    redirect short urls to the approproate long url

--- express_server.js
+++ express_server.js
@@ -35,14 +35,24 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.get("/urls/:id", (req, res) => {
+app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  console.log(req.body);
+  res.send("Ok");
+
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = { longURL: req.body["longUrl"] };
+
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.listen(PORT, () => {
